b50a5b82d59bde6703d9ed8d030a867bba72b47b
Sean Wong <seanwong24@live.com>
2023-02-21 01:02:34 +0000

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+    },
+  },
 });
